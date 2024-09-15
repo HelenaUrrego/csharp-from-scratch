f4329106e6ed70949b99d5977fd9fe23c307e218
Helena Urrego <hlnaurrego@gmail.com>
2024-09-15 14:11:16 -0500

    Change hello world by class

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace HelloWorld{                                //como se llama el proyecto.el nombre de la carpeta donde esta
+   class Program{
+
+    static void Main(){
+         var Name="Jhon";
+        Name="1Jhon";
+        Console.WriteLine($"Hello world! {Name}");
+        Console.WriteLine();
+    }
+}
+}
